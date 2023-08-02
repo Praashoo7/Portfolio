09235ac7a850c84cf41204901ed1cdef02243a97
Prashant <rnttl90@gmail.com>
2023-08-03 00:17:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -383,7 +383,7 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 100;
+    var elementVisible = 0;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
