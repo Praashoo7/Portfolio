6a135b46112effeff04b798a1c6e3a119cac7957
Prashant <rnttl90@gmail.com>
2023-08-05 02:39:38 +0530

    coin

--- index.js
+++ index.js
@@ -377,6 +377,26 @@ window.addEventListener("scroll", reveal04);
 window.addEventListener("DOMContentLoaded", reveal04);
 
 
+function revealM() {
+  var reveals = document.querySelectorAll(".revealM");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = -10;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealM);
+window.addEventListener("DOMContentLoaded", revealM);
+
+
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
@@ -454,3 +474,221 @@ function reveal111() {
 
 window.addEventListener("scroll", reveal111);
 window.addEventListener("DOMContentLoaded", reveal111);
+
+
+/*<!---------------------------------------------------------------------- Click-Me ---------------------------------------------------------------------->*/
+
+
+const animateButton = document.getElementById('ic');
+var y = document.getElementById('las');
+var clickCounter = 0;
+
+animateButton.addEventListener('click', () => {
+    // Create a clone of the original animatedBox
+    const animatedBox = document.getElementById('coin');
+    const clone = animatedBox.cloneNode(true);
+
+    // Append the clone to the parent element
+    animatedBox.parentNode.appendChild(clone);
+
+    // Remove the "animate" class if it already exists (from the clone, not the original)
+    clone.classList.remove('animate');
+
+    // Trigger the reflow by reading the offsetHeight property
+    // This is to ensure the animation starts from the beginning
+    void clone.offsetWidth;
+
+    // Add the "animate" class to the clone to trigger the animation
+    clone.classList.add('animate');
+
+    // Remove the clone after the animation is complete (optional)
+    clone.addEventListener('animationend', () => {
+      clone.remove();
+    });
+
+    clickCounter++;
+
+  
+    if (clickCounter === 10) {
+      showMessage("Oh, you like the Animation!", 3000);
+    } else if (clickCounter === 35) {
+      showMessage("I guess you can stop Now. How many Coins do you Need?", 3000);
+    } else if (clickCounter === 75) {
+      showMessage("You really clicked 75 times!", 3000);
+    } else if (clickCounter === 100) {
+      showMessage("You are at a Whooping 100 now!", 3000);
+    } else if (clickCounter === 140) {
+      showMessage("140! Feel mature enough?", 3000);
+    } else if (clickCounter === 170) {
+      showMessage1("01011001 01101111 01110101 01010010 01100101 01100001 01101100 01101100 01111001 01001000 01100001 01110110 01100101 01010100 01101001 01101101 01100101", 5000);
+    } else if (clickCounter === 200) {
+      showRedditLinkMessage();
+    } else if (clickCounter === 250) {
+      showGiftMessage();
+      document.getElementById("ic").style.pointerEvents = "none";
+    }
+  });
+  
+  // Function to display the message and progress bar
+  function showMessage(message, duration) {
+
+    var x = document.getElementById("amessage");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+
+    const messageElement = document.getElementById("amessage");
+    messageElement.textContent = message;
+  
+    const progressBar = document.createElement("div");
+    progressBar.className = "progress-bar";
+    messageElement.appendChild(progressBar);
+
+    const startTime = Date.now();
+    function animate() {
+      const elapsedTime = Date.now() - startTime;
+      const progress = Math.min(1, elapsedTime / duration);
+      progressBar.style.width = `${progress * 100}%`;
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      } else {
+        progressBar.remove();
+        messageElement.textContent = "";
+      }
+    }
+
+    requestAnimationFrame(animate);
+}
+
+// Function to display the message and progress bar
+function showMessage1(message, duration) {
+
+  var x = document.getElementById("amessage");
+  x.className = "show1";
+  setTimeout(function(){ x.className = x.className.replace("show1", ""); }, 5000);
+
+  const messageElement = document.getElementById("amessage");
+  messageElement.textContent = message;
+
+  const progressBar = document.createElement("div");
+  progressBar.className = "progress-bar";
+  messageElement.appendChild(progressBar);
+
+  const startTime = Date.now();
+  function animate() {
+    const elapsedTime = Date.now() - startTime;
+    const progress = Math.min(1, elapsedTime / duration);
+    progressBar.style.width = `${progress * 100}%`;
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      progressBar.remove();
+      messageElement.textContent = "";
+    }
+  }
+
+  requestAnimationFrame(animate);
+}
+  
+  // Function to show the Reddit link message
+  function showRedditLinkMessage() {
+    const duration = 5000;
+
+    var x = document.getElementById("amessage");
+    x.className = "show1";
+    setTimeout(function(){ x.className = x.className.replace("show1", ""); }, 5000);
+  
+    // Create the anchor element
+    const link = document.createElement("a");
+    link.href = "https://www.reddit.com/r/Showerthoughts/comments/a1f6xu/you_have_too_much_free_time_is_an_insult_even/?utm_source=share&utm_medium=web2x&context=3";
+    link.target = "_blank";
+    link.textContent = "Reddit🔗";
+
+    // Create a text node for the introductory text
+    const textNode = document.createTextNode("Check out this ");
+
+    // Get the "amessage" element
+    const amessageElement = document.getElementById("amessage");
+
+    // Clear any existing content in "amessage" (optional, depending on your use case)
+    amessageElement.textContent = "";
+
+    // Append the text node and the anchor element as children to "amessage" in the desired order
+    amessageElement.appendChild(textNode);
+    amessageElement.appendChild(link);
+    amessageElement.appendChild(document.createTextNode(" thing."));
+
+     // Create the progress bar element
+    const progressBar = document.createElement("div");
+    progressBar.className = "progress-bar";
+    amessageElement.appendChild(progressBar);
+
+    const startTime = Date.now();
+    function animate() {
+      const elapsedTime = Date.now() - startTime;
+      const progress = Math.min(1, elapsedTime / duration);
+      progressBar.style.width = `${progress * 100}%`;
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      } else {
+        progressBar.remove();
+        amessageElement.textContent = ""; // Clear the entire message after the progress bar completes
+      }
+    }
+
+    requestAnimationFrame(animate);
+    
+  }
+  
+  // Function to show the gift message
+  function showGiftMessage() {
+    const duration = 20000;
+
+    var x = document.getElementById("amessage");
+    x.className = "show2";
+    setTimeout(function(){ x.className = x.className.replace("show2", ""); }, 20000);
+  
+    // Create the anchor element for the link
+    const link = document.createElement("a");
+    link.href = "https://anothermetosee.github.io/Data/";
+    link.target = "_blank";
+    link.textContent = "Winning Prize🎁";
+
+    // Construct the text content with placeholders for the click count and add the link at the end
+    const clickCount = 250;
+    const text = `Well, You have passed the Test. I am really Impressed, You clicked total ${clickCount} times. Here is your `;
+
+    // Get the "amessage" element
+    const amessageElement = document.getElementById("amessage");
+
+    // Clear any existing content in "amessage" (optional, depending on your use case)
+    amessageElement.textContent = "";
+
+    // Create a text node with the combined text and link content
+    const combinedContent = document.createTextNode(text);
+    amessageElement.appendChild(combinedContent);
+    amessageElement.appendChild(link);
+
+    // Add the closing text
+    amessageElement.appendChild(document.createTextNode(". Adios 👋."));
+
+     // Create the progress bar element
+  const progressBar = document.createElement("div");
+  progressBar.className = "progress-bar";
+  amessageElement.appendChild(progressBar);
+
+  const startTime = Date.now();
+  function animate() {
+    const elapsedTime = Date.now() - startTime;
+    const progress = Math.min(1, elapsedTime / duration);
+    progressBar.style.width = `${progress * 100}%`;
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      progressBar.remove();
+      amessageElement.textContent = ""; // Clear the entire message after the progress bar completes
+    }
+  }
+
+  requestAnimationFrame(animate);
+
+}
